208ebe2804f63a68bf5e91e0d05d66e496ec2877
Michael Malleske <mmalleske@gmail.com>
2016-06-10 16:58:39 -0500

    working without winning condition

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 $( document ).ready(function() {
 //mouseover color change
-  var clicked = false;
+  var playerOne = true;
   $('.oPiece').hide(0);
+  $('.xPiece').hide(0);
   $('.cell').mouseenter(function() {
-    $( this ).children('a').css( "background-color", "#57c4d0" );
+    if (playerOne){
+      $( this ).children('a').css( "background-color", "#57c4d0" );
+    }
+    else{
+      $( this ).children('a').css( "background-color", "#990099" );
+    }
   });
   $('.cell').mouseleave(function() {
-    $( this ).children('a').css( "background-color", "cellColor" );
+    $( this ).children('a').css( "background-color", "orange" );
   });
 //click
   $('.cell').click(function(e) {
     e.preventDefault();
-    clicked = true;
-    $( this ).children('a').hide(200);
-    $( this ).children('span').show(200);
-    $('.oPiece').css("background-color, #57c4d0");
+    $( this ).children('a').hide();
+    if(playerOne){
+      $( this ).children('.oPiece').show(200);
+    }
+    else if (playerOne===false){
+      $( this ).children('.xPiece').show(200);
+    }
+    $( this ).unbind();
+    playerOne = !playerOne;
   });
 
 
