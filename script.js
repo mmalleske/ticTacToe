601c6ae88b33a2355fba0539424c764ecf27dc1f
Michael Malleske <mmalleske@gmail.com>
2016-06-10 15:39:01 -0500

    game pieces using show/hide

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 $( document ).ready(function() {
 //mouseover color change
   var clicked = false;
-  var cellColor = "orange";
+  $('.oPiece').hide(0);
   $('.cell').mouseenter(function() {
     $( this ).children('a').css( "background-color", "#57c4d0" );
   });
   $('.cell').mouseleave(function() {
-    $( this ).children('a').css( "background-color", cellColor );
+    $( this ).children('a').css( "background-color", "cellColor" );
   });
 //click
   $('.cell').click(function(e) {
     e.preventDefault();
     clicked = true;
-    //cellColor = "white";
     $( this ).children('a').hide(200);
-    $( this ).append('<span class="oPiece" style="background-color: white; color:#57c4d0;  padding : 2px 10px; font-size: 40px; border: 3px solid #57c4d0; border-radius: 4px; text-align: center;">O</span>');
+    $( this ).children('span').show(200);
     $('.oPiece').css("background-color, #57c4d0");
   });
 
